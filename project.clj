b991aedd0b8b08827c825c6df7bc350546ca470d
Alexander Sanchez <alex@harbinger.localdomain>
2021-05-11 17:13:36 -0700

    Add license information.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject cork/warp "0.1.0-SNAPSHOT"
+  :description "Simple Parsing Library"
+  :url "https://gitlab.com/cork.place/cork.warp"
+  :license {:name    "GPL-3.0"
+            :comment "GNU General Public License v3.0"
+            :url     "https://choosealicense.com/licenses/gpl-3.0"
+            :year    2021
+            :key     "gpl-3.0"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :plugins [[lein-license "1.0.0"]])
